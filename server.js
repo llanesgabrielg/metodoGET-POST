b007ed787aeb369c50598f52e0da92b797ab37b7
gabriel <llanesgabrielg@gmail.com>
2021-05-13 15:11:17 -0300

    server que responde a solicitudes GET-POST

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
+//forma de indicar ruta absoluta
+const path = require("path");
 const app = express();
-app.get('/',function(req,res){
-    res.send('hi , try send message');
+//middleware
+// procesos en el medio, body parser , decodifica lo que llega del formulario , lo transforma en un objeto y lo deja disponible en el objeto body
+app.use(express.urlencoded({extended:true}));
+
+//endPoint
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+app.post('/saludar',function(req,res){
+    console.log(req.body);
+    res.send(`hola ${req.body.name} "${req.body.nickName}" ${req.body.lastName}`);
 });
 
-app.listen(3000,function(){
-    console.log('server on');
+app.listen(3000, function () {
+  console.log("server on");
 });
